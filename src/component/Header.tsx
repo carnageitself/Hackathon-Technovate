f5758b859b9c05a67e151452063ba35f577e84eb
GODCarNageOP <yashvharale@gmail.com>
2023-09-16 20:27:49 +0530

    header updated

--- src/component/Header.tsx
+++ src/component/Header.tsx
@@ -17,7 +17,7 @@ function Header() {
                             setShow(false)
                         }
                     }} /></div>
-            <ul className=' font-normal  font-sans sm:flex hidden gap-10 text-gray-700' style={{ fontFamily: 'Poppins' }}>
+            <ul className=' font-normal  font-sans sm:flex hidden gap-10 text-gray-700'>
                 <li className='border-b-2 font-semibold text-xl border-[#EE619C] w-20 flex justify-center'> <a href="/"> Home</a></li>
 
                 <li className='border-b-2 font-semibold text-xl border-[#EE619C] w-20 flex justify-center'> <a href="/about"> About</a></li>
